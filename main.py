ebd8dc3cdae73d2ccc097aea8f0d79e14e1a7cd7
Amr <sherie1165@gmail.com>
2020-11-20 22:57:01 +0200

    added the ability to normally rename a file

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 import os
+
+
+print("Please one of these options:")
+print("1: Rename the file normally")
+print("2: Rename multiple files in a sequence")
+choice = int(input())
+
+if choice == 1:
+    print("Please insert the full path for the file to rename:")
+    old_path = input()
+    extension_list = old_path.split(".")
+    extension = "." + str(extension_list[len(extension_list) - 1])
+    print("Please insert the new name for the file with the " + extension)
+    new_name = input()
+    directory_split_list = old_path.rsplit('\\', 1)
+    new_path = str(directory_split_list[0]) + "\\" + str(new_name)
+    os.rename(old_path, new_path)
