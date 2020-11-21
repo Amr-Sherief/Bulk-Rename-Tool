a20c5e0de54422d0aadbffe90c0fcd57ea4e121b
Amr <sherie1165@gmail.com>
2020-11-21 22:55:52 +0200

    added the ability to rename multiple files at once

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ while True:
         directory_split_list = old_path.rsplit('\\', 1)
         new_path = str(directory_split_list[0]) + "\\" + str(new_name)
         os.rename(old_path, new_path)
+        break
 
     elif choice == 2:
         print("Please insert the file path:")
@@ -26,10 +27,26 @@ while True:
         print("2: rename certain files in the folder")
         choice_2 = int(input())
         if choice_2 == 1:
-
-        if choice_2 == 2:
-
-        else:
+            files = os.listdir(file_path)
+            print("Please insert the new name for the files with no numbers")
+            name = str(input())
+            print("Please insert the number to start with")
+            number = int(input())
+            print("Please insert the character between " + name + " " + str(number))
+            splitter = str(input())
+            for file in files:
+                extension_list = file.rsplit(".", 1)
+                extension = "." + extension_list[len(extension_list) - 1]
+                new_name = name + splitter + str(number) + extension
+                new_path = file_path + "\\" + new_name
+                old_path = file_path + "\\" + file
+                os.rename(old_path, new_path)
+                number = number + 1
+                if file == files[len(files) - 1]:
+                    print("Done!")
+            break
+        elif choice_2 == 2:
+            files = os.listdir(file_path)
 
     else:
         continue
